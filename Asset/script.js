5f32af76da85c2017faaf60036b7033e2704366c
Philip <Pjurkin@gmail.com>
2023-11-24 17:55:48 -0600

    started some stuff

---
+++ Asset/script.js
@@ -0,0 +1,31 @@
+var timeEl = document.querySelector(".timeelement");
+var questionEl = document.querySelector(".questionelement");
+var answerEl = document.querySelector(".answerelement");
+const quizQuestions = [{
+
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris", "Rome"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        choices: ["Jupiter", "Mars", "Neptune", "Mercury"],
+        correctAnswer: "Mars"
+    }, {
+        question: "Who painted the Mona Lisa?",
+        choices: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
+        correctAnswer: "Leonardo da Vinci"
+    }, {
+        question: "What is the largest mammal in the world?",
+        choices: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+        correctAnswer: "Blue Whale"
+    }, {
+        question: "What is the closest star to Earth?",
+        choices: ["Proxima Centauri", "Alpha Centauri", "Sirius", "Sun"],
+        correctAnswer: "Sun"
+    }, {
+        question: "What year did World War I begin?",
+        choices: ["1914", "1918", "1939", "1905"],
+        correctAnswer: "1914"
+    }
+];
